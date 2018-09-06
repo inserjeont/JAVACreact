4b06a08aa5897140f0c4ab452b8e1466e24b7490
user <user@JBTC10102>
2018-09-06 14:04:27 +0900

    906

---
+++ 자바기초실습/src/제03차시/실습05.java
@@ -0,0 +1,27 @@
+package 제03차시;
+
+public class 실습05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[];
+		a=new int[5];
+		a[0]=10;
+		a[1]=15;
+		a[2]=20;
+		a[3]=23;
+		a[4]=35;
+		
+		int b[][]= {
+					{1,2,3},
+					{4,5,6},
+					{7,8,9},
+					{10,11,12}
+					};
+		
+		System.out.println("배열 a[]의 크기: "+ a.length);
+		System.out.println("배열 b[][]의 크기: "+ b.length);
+		System.out.println("배열 b[1]의 크기: "+b[1].length);
+	}
+
+}
