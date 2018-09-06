30d88a4d9b16e1decc9e98492edc531ac7c704a5
user <user@JBTC10102>
2018-09-06 14:06:51 +0900

    906

--- 자바기초실습/src/제03차시/실습05.java
+++ 자바기초실습/src/제03차시/실습05.java
@@ -13,15 +13,17 @@ public class 실습05 {
 		a[4]=35;
 		
 		int b[][]= {
-					{1,2,3},
-					{4,5,6},
-					{7,8,9},
-					{10,11,12}
+						{1,2,3},
+						{4,5,6},
+						{7,8,9},
+						{10,11,12},
+						{13,14}
 					};
 		
 		System.out.println("배열 a[]의 크기: "+ a.length);
 		System.out.println("배열 b[][]의 크기: "+ b.length);
 		System.out.println("배열 b[1]의 크기: "+b[1].length);
+		System.out.println("배열 b[4]의 크기: "+b[4].length);
 	}
 
 }
