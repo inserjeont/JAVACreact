9dff7adeb56b37348be495cb29622fa9e0ea123b
inserjeont <42628260+inserjeont@users.noreply.github.com>
2018-09-10 16:24:32 +0900

    자바
    자바

---
+++ 자바기초실습/src/자바코드업/Main1030.java
@@ -0,0 +1,14 @@
+package ÄÚµå¾÷;
+
+import java.util.Scanner;
+
+public class Main1030 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a;
+		a = sc.nextLong();
+		sc.close();
+		System.out.printf("%d",a);
+	}
+}
